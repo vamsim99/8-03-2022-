1e31c5b58502de92eeb4525c7a17da12f4196864
vamsi <vamsimappetti@gmail.com>
2022-03-03 11:11:49 +0530

    swap.py

--- swap.py
+++ swap.py
@@ -1,13 +1,9 @@
-# Python program to swap two variables
+
 
 x = 5
 y = 10
 
-# To take inputs from the user
-#x = input('Enter value of x: ')
-#y = input('Enter value of y: ')
 
-# create a temporary variable and swap the values
 temp = x
 x = y
 y = temp
